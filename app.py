69b9d0fcf74adc7bd08421860c96e06bb033cad8
hayatoy <hayatovip@gmail.com>
2022-06-09 14:41:30 +0900

    add IconLayer

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
 import threading
 import time
 import streamlit as st
-import altair as alt
-from vega_datasets import data
 from queue import Queue
 from google.cloud import firestore
-from streamlit.components.v1 import html
 import pandas as pd
 import numpy as np
 import pydeck as pdk
-import logging
+
 
 db = firestore.Client()
 q = Queue()
@@ -22,8 +19,26 @@ st.title('Firestore Demo')
 st.subheader('Current Locations')
 
 # create blank DataFrame
-df_trip = pd.DataFrame(columns=['path', 'timestamps'], data=None)
+df_trip = pd.DataFrame(columns=['path', 'last_pos', 'icon_data', 'timestamps'], data=None)
 
+ICON_URL = 'https://raw.githubusercontent.com/hayatoy/cloudrun-mobility-dashboard/main/car.png'
+icon_data = {
+  "url": ICON_URL,
+  "width": 128,
+  "height": 128,
+  "anchorY": 96,
+  "anchorX": 64
+}
+
+icon_layer = pdk.Layer(
+  'IconLayer',
+  data=df_trip,
+  pickable=True,
+  get_icon='icon_data',
+  get_size=4,
+  size_scale=12,
+  get_position="last_pos"
+)
 
 trip_layer = pdk.Layer(
   'TripsLayer',
@@ -34,7 +49,7 @@ trip_layer = pdk.Layer(
   opacity=1,
   width_min_pixels=3,
   rounded=True,
-  trail_length=8,
+  trail_length=120,
   current_time=100,
 )
 
@@ -49,7 +64,7 @@ r = pdk.Deck(
   #  tilt=45
     pitch=50,
   ),
-  layers=[trip_layer],
+  layers=[trip_layer, icon_layer],
 )
 rt_map = st.pydeck_chart(r)
 
@@ -76,12 +91,12 @@ def main():
   # Watch the document
   col_watch = col_ref.on_snapshot(on_snapshot)
 
-  df_trip = pd.DataFrame(columns=['path', 'timestamps'], data=None)
+  df_trip = pd.DataFrame(columns=['path', 'last_pos', 'icon_data', 'timestamps'], data=None)
   while True:
     doc_list = q.get()
 
-    if doc_list[0]['timestamp'] == 0:
-      df_trip = pd.DataFrame(columns=['path', 'timestamps'], data=None)
+    if doc_list[0]['timestamp'] <= 10:
+      df_trip = pd.DataFrame(columns=['path', 'last_pos', 'icon_data', 'timestamps'], data=None)
 
     for doc in doc_list:
       vehicle_ind = doc['id']
@@ -92,16 +107,19 @@ def main():
         _path = []
       _path.append(doc['lonlat'])
       df_trip.at[vehicle_ind, 'path'] = _path
+      df_trip.at[vehicle_ind, 'last_pos'] = doc['lonlat']
+      df_trip.at[vehicle_ind, 'icon_data'] = icon_data
 
       _timestamps = df_trip.at[vehicle_ind, 'timestamps']
       if type(_timestamps) != list:
         _timestamps = []
       _timestamps.append(doc['timestamp'])
       df_trip.at[vehicle_ind, 'timestamps'] = _timestamps
-    
+
     snap.write(doc_list)
     trip_layer.current_time = doc_list[-1]['timestamp']
     trip_layer.data = df_trip
+    icon_layer.data = df_trip
     r.update()
     rt_map.pydeck_chart(r)
 
